a3599d1a3c4c87a416465d676b5a79eeb709ca99
ssar@nate.com <ssar@nate.com>
2021-12-21 19:58:28 +0900

    코드 에이전트 재시작함

--- deploy.sh
+++ deploy.sh
@@ -26,7 +26,7 @@ rm -rf $REPOSITORY/$PROJECT_NAME
 mkdir $REPOSITORY/$PROJECT_NAME
 cp $DEPLOY_DIRECTORY/build/libs/*.jar $REPOSITORY/$PROJECT_NAME/blog-server-repository.jar
 
-echo "4. jar파일 실행권한 주고 nohup 으로 실행하기 - 출력 재지정(Redirect) 안하면 오류남!!"
+echo "4. jar파일 실행권한 주고 nohup 으로 실행하기 - 출력 재지정(Redirect) 안하면 오류남!"
 
 chmod +x $JAR_NAME
 nohup java -jar -Dspring.profiles.active=prod -Dfile.encoding=UTF-8  $JAR_NAME > $REPOSITORY/$PROJECT_NAME/nohup.out 2>&1 &
